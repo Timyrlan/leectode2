c55b51073f1ba267fa1a8f79bc8b74921c02bdfa
lemeshko@topcase.ru <Rzhd.Lkm.SyncSolution.API@itopcase.ru>
2025-02-11 21:41:49 +0300

    Solution_1431_Kids_With_the_Greatest_Number_of_Candies

---
+++ Solutions/Solution_1431_Kids_With_the_Greatest_Number_of_Candies.cs
@@ -0,0 +1,10 @@
+namespace Solutions;
+
+public class Solution_1431_Kids_With_the_Greatest_Number_of_Candies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var minRequired = candies.Max() - extraCandies;
+        return candies.Select(c => c >= minRequired).ToArray();
+    }
+}
--- Solutions/UnitTest.cs
+++ Solutions/UnitTest.cs
@@ -18,6 +18,7 @@ public class Tests
     {
         Assert.That(new Solution_1071_Greatest_Common_Divisor_of_Strings().GcdOfStrings(param1, param2), Is.EqualTo(expected));
     }
+
     [TestCase("ABC", "ABCABC", "ABC")]
     [TestCase("AB", "ABABAB", "ABAB")]
     [TestCase("", "LEET", "CODE")]
@@ -25,5 +26,13 @@ public class Tests
     public void Test_1071_Greatest_Common_Divisor_of_Strings2(string expected, string param1, string param2)
     {
         Assert.That(new Solution_1071_Greatest_Common_Divisor_of_Strings2().GcdOfStrings(param1, param2), Is.EqualTo(expected));
+    }
+
+    [TestCase(new[] { true, true, true, false, true }, new[] { 2, 3, 5, 1, 3 }, 3)]
+    [TestCase(new[] { true, false, false, false, false }, new[] { 4, 2, 1, 1, 2 }, 1)]
+    [TestCase(new[] { true, false, true }, new[] { 12, 1, 12 }, 10)]
+    public void Solution_1431_Kids_With_the_Greatest_Number_of_Candies(bool[] expected, int[] candies, int extraCandies)
+    {
+        Assert.That(new Solution_1431_Kids_With_the_Greatest_Number_of_Candies().KidsWithCandies(candies, extraCandies), Is.EqualTo(expected.ToList()));
     }
 }
