1409e0473f65833e3fa9d47038776748f02682bc
lemeshko@topcase.ru <Rzhd.Lkm.SyncSolution.API@itopcase.ru>
2025-03-10 23:10:44 +0300

    437. Path Sum III

--- Solutions/Medium/BinaryTree/Solution_437_Path_Sum_III.cs
+++ Solutions/Medium/BinaryTree/Solution_437_Path_Sum_III.cs
@@ -2,8 +2,64 @@ namespace Solutions.Medium.BinaryTree;
 
 public class Solution_437_Path_Sum_III
 {
+    private readonly Dictionary<long, int> dic = new();
+    private readonly Stack<Tuple<TreeNode, long>> stack = new();
+    private int countArrays;
+    private int TargetSum;
+
     public int PathSum(TreeNode root, int targetSum)
     {
-        throw new NotImplementedException();
+        if (root == null) return 0;
+
+        //if (root.left == null && root.right == null)
+        //{
+        //    return targetSum == root.val ? 1 : 0;
+        //}
+
+        TargetSum = targetSum;
+
+        //dic.Add(0, 1);
+
+        ProcessNode(0, root);
+
+        while (stack.Any())
+        {
+            var node = stack.Peek();
+
+            if (node.Item1.left != null)
+            {
+                ProcessNode(node.Item2, node.Item1.left);
+                node.Item1.left = null;
+            }
+            else if (node.Item1.right != null)
+            {
+                ProcessNode(node.Item2, node.Item1.right);
+                node.Item1.right = null;
+            }
+            else if (node.Item1.left == null && node.Item1.right == null)
+            {
+                node = stack.Pop();
+                dic[node.Item2]--;
+            }
+        }
+
+        return countArrays;
+    }
+
+    private void ProcessNode(long prevSum, TreeNode node)
+    {
+        var sum = prevSum + node.val;
+
+        var dif = sum - TargetSum;
+
+        if (sum == TargetSum) 
+            countArrays++;
+
+        if (dic.TryGetValue(dif, out var countEntries)) 
+            countArrays += countEntries;
+
+        if (!dic.TryAdd(sum, 1)) dic[sum]++;
+
+        stack.Push(new Tuple<TreeNode, long>(node, sum));
     }
 }
--- Solutions/Medium/BinaryTree/Test.cs
+++ Solutions/Medium/BinaryTree/Test.cs
@@ -16,10 +16,68 @@ public class Test
     public void Solution_437_Path_Sum_III()
     {
         var targetSum = 8;
-        var node1 = new TreeNode(3, new TreeNode(1, new TreeNode(3)), new TreeNode(4, new TreeNode(1), new TreeNode(5)));
+        var node1 = new TreeNode(10, new TreeNode(5, new TreeNode(3, new TreeNode(3), new TreeNode(-2)), new TreeNode(2, new TreeNode(1))),
+            new TreeNode(-3, new TreeNode(11))
+        );
+
+
+        Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Solution_437_Path_Sum_III_2()
+    {
+        var targetSum = 22;
+        var node1 = new TreeNode(5,
+            new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+            new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1)))
+        );
+
+
+        Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Solution_437_Path_Sum_III_3()
+    {
+        var targetSum = 0;
+        var node1 = new TreeNode(1);
+
+
+        Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(0));
+    }
+
+
+    [Test]
+    public void Solution_437_Path_Sum_III_4()
+    {
+        var targetSum = 0;
+        var node1 = new TreeNode(1, new TreeNode(2));
+
+
+        Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(0));
+    }
+
+
+    [Test]
+    public void Solution_437_Path_Sum_III_5()
+    {
+        var targetSum = 1;
+        var node1 = new TreeNode(0, new TreeNode(1), new TreeNode(1));
 
 
         Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(4));
+    }
+
+
+    [Test]
+    public void Solution_437_Path_Sum_III_6()
+    {
+        var targetSum = 0;
+        var node1 = new TreeNode(1000000000, new TreeNode(1000000000,new TreeNode(294967296, new TreeNode(1000000000, new TreeNode(1000000000, new TreeNode(1000000000))))));
+
+
+        Assert.That(new Solution_437_Path_Sum_III().PathSum(node1, targetSum), Is.EqualTo(0));
     }
 }
 
